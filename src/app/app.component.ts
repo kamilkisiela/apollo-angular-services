8b106b91693d6d5c571146075f88c8e276d35fb5
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-08-18 16:33:27 +0200

    Reset

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+
+  `,
 })
-export class AppComponent {
-  title = 'apollo-services-example';
-}
+export class AppComponent {}
