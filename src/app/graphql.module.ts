6b0c776d3c1ccf5720ec77dff9436636fac4c669
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-09-18 12:11:42 +0200

    Set endpoint in graphql config

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -5,7 +5,6 @@ import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
-// GraphiQL: https://simple-posts-authors-graphql.glitch.me
 const uri = 'https://simple-posts-authors-graphql.glitch.me';
 
 @NgModule({
